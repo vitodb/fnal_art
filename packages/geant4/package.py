66b90d7660915558a171d4d114821525559f4630
Patrick Gartung <gartung@fnal.gov>
2019-02-05 14:01:24 -0600

    Sync up with builtin.geant4 but keep Fermilab options.

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -17,23 +17,61 @@ class Geant4(CMakePackage):
     homepage = "http://geant4.cern.ch/"
     url = "http://geant4.cern.ch/support/source/geant4.10.01.p03.tar.gz"
 
-    version('10.03.p03', 'ccae9fd18e3908be78784dc207f2d73b')
+    version('10.04', 'b84beeb756821d0c61f7c6c93a2b83de')
+    version('10.03.p03', 'ccae9fd18e3908be78784dc207f2d73b', preferred=True)
+    version('10.02.p03', '2b887e66f0d41174016160707662a77b')
+    version('10.02.p02', '6aae1d0fc743b0edc358c5c8fbe48657')
+    version('10.02.p01', 'b81f7082a15f6a34b720b6f15c6289cfe4ddbbbdcef0dc52719f71fac95f7f1c')
+    version('10.01.p03', '4fb4175cc0dabcd517443fbdccd97439')
+
+    variant('qt', default=False, description='Enable Qt support')
+    variant('vecgeom', default=False, description='Enable vecgeom support')
+    variant('opengl', default=False, description='Optional OpenGL support')
+    variant('x11', default=False, description='Optional X11 support')
+    variant('motif', default=False, description='Optional motif support')
+    variant('threads', default=True, description='Build with multithreading')
 
     variant('cxxstd',
-            default='17',
-            values=('14', '17'),
+            default='11',
+            values=('11', '14', '17'),
             multi=False,
             description='Use the specified C++ standard when building.')
 
-    variant('qt', default=False, description='Enable Qt support')
-    variant('vecgeom', default=False, description='Enable Vecgeom support')
-
     depends_on('cmake@3.5:', type='build')
 
-    depends_on("clhep")
+    # C++11 support
+    depends_on("clhep@2.4.0.0 cxxstd=11", when="@10.04 cxxstd=11")
+    depends_on("clhep@2.3.4.3 cxxstd=11", when="@10.03.p03 cxxstd=11")
+    depends_on("clhep@2.3.1.1 cxxstd=11", when="@10.02.p01 cxxstd=11")
+    depends_on("clhep@2.3.1.1 cxxstd=11", when="@10.02.p01 cxxstd=11")
+    depends_on("clhep@2.2.0.4 cxxstd=11", when="@10.01.p03 cxxstd=11")
+    depends_on("vecgeom cxxstd=11", when="+vecgeom cxxstd=11")
+
+    # C++14 support
+    depends_on("clhep@2.4.0.0 cxxstd=14", when="@10.04 cxxstd=14")
+    depends_on("clhep@2.3.4.3 cxxstd=14", when="@10.03.p03 cxxstd=14")
+    depends_on("clhep@2.3.1.1 cxxstd=14", when="@10.02.p02 cxxstd=14")
+    depends_on("clhep@2.3.1.1 cxxstd=14", when="@10.02.p01 cxxstd=14")
+    depends_on("clhep@2.2.0.4 cxxstd=14", when="@10.01.p03 cxxstd=14")
+    depends_on("vecgeom cxxstd=14", when="+vecgeom cxxstd=14")
+
+    # C++17 support
+    patch('cxx17.patch', when='cxxstd=17')
+    depends_on("clhep@2.4.0.0 cxxstd=17", when="@10.04 cxxstd=17")
+    depends_on("clhep@2.3.4.3 cxxstd=17", when="@10.03.p03 cxxstd=17")
+    depends_on("clhep@2.3.1.1 cxxstd=17", when="@10.02.p02 cxxstd=17")
+    depends_on("clhep@2.3.1.1 cxxstd=17", when="@10.02.p01 cxxstd=17")
+    depends_on("clhep@2.2.0.4 cxxstd=17", when="@10.01.p03 cxxstd=17")
+    depends_on("vecgeom cxxstd=17", when="+vecgeom cxxstd=17")
+
     depends_on("expat")
+    depends_on("zlib")
     depends_on("xerces-c")
-    depends_on("qt@4.8:4.9999", when="+qt")
+    depends_on("mesa", when='+opengl')
+    depends_on("libx11", when='+x11')
+    depends_on("libxmu", when='+x11')
+    depends_on("motif", when='+motif')
+    depends_on("qt@4.8:4.999", when="+qt")
     # G4 data
     depends_on("geant4-g4abla")
     depends_on("geant4-g4emlow")
@@ -46,9 +84,6 @@ class Geant4(CMakePackage):
     depends_on("geant4-g4surface")
     depends_on("geant4-g4tendl")
 
-
-    patch('cxx17.patch', when='cxxstd=17')
-
     def cmake_args(self):
         spec = self.spec
 
@@ -60,22 +95,32 @@ class Geant4(CMakePackage):
 			     '-DXERCESC_ROOT_DIR:STRING="%s"' % self.spec['xerces-c'].prefix 
                   ]
 
-        options.append('-DGEANT4_BUILD_CXXSTD=%s' % self.spec.variants['cxxstd'].value)
+        arch = platform.system().lower()
+        if arch != 'darwin':
+            if "+x11" in spec and "+opengl" in spec:
+                options.append('-DGEANT4_USE_OPENGL_X11=ON')
+            if "+motif" in spec and "+opengl" in spec:
+                options.append('-DGEANT4_USE_XM=ON')
+            if "+x11" in spec:
+                options.append('-DGEANT4_USE_RAYTRACER_X11=ON')
 
+        options.append('-DGEANT4_BUILD_CXXSTD=c++%s' %
+                       self.spec.variants['cxxstd'].value)
 
         if '+qt' in spec:
             options.append('-DGEANT4_USE_QT=ON')
             options.append(
                 '-DQT_QMAKE_EXECUTABLE=%s' %
-                spec['qt'].prefix + '/bin/qmake'
-            )
+                spec['qt'].prefix.bin.qmake)
 
         if '+vecgeom' in spec:
             options.append('-DGEANT4_USE_USOLIDS=ON')
-            options.append('-DUSolids_DIR=%s' %
-                           join_path(spec['vecgeom'].prefix, 'lib/CMake/USolids'))
+            options.append('-DUSolids_DIR=%s' % spec[
+                'vecgeom'].prefix.lib.CMake.USolids)
 
- 
+        on_or_off = lambda opt: 'ON' if '+' + opt in spec else 'OFF'
+        options.append('-DGEANT4_BUILD_MULTITHREADED=' + on_or_off('threads'))
+
         return options
 
     def url_for_version(self, version):
