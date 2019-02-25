fa1da545ecaddfa16e1a8cb3b8ef41baa68f5ec6
Patrick Gartung <gartung@fnal.gov>
2019-02-25 17:08:51 -0600

    Add trace and artdaq_core.

---
+++ packages/artdaq_core/package.py
@@ -0,0 +1,43 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class ArtdaqCore(CMakePackage):
+    """The toolkit currently provides functionality for data transfer,
+ event building, event reconstruction and analysis (using the art analysis
+ framework), process management, system and process state behavior, control
+ messaging, local message logging (status and error messages), DAQ process 
+ and art module configuration, and the writing of event data to disk in ROOT
+ format."""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/artdaq/wiki"
+    url      = "http://cdcvs.fnal.gov/projects/artdaq-core/"
+
+    version('develop', git = url, branch="develop")
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+    patch('patch')
+
+    depends_on('cetmodules', type='build')
+    depends_on('canvas_root_io')
+    depends_on('canvas')
+    depends_on('messagefacility')
+    depends_on('root')
+    depends_on('boost')
+    depends_on('trace')
+ 
+    def cmake_args(self):
+        args = ['-DCMAKE_CXX_STANDARD={0}'.
+                format(self.spec.variants['cxxstd'].value),
+                '-DCANVAS_VERSION=1.2.3',
+                '-DMESSAGEFACILITY_VERSION=1.2.3',
+               ]
+        return args
---
+++ packages/trace/package.py
@@ -0,0 +1,30 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack import *
+
+
+class Trace(CMakePackage):
+    """TRACE is yet another logging (time stamp) tool, but it allows 
+fast and/or slow logging - dynamically (you choose)."""
+
+    homepage = "https://cdcvs.fnal.gov/redmine/projects/trace/wiki"
+    url      = "http://cdcvs.fnal.gov/projects/trace-git/"
+
+    parallel = False 
+
+    version('3.13.11', git=url, branch='master')
+
+    patch('patch')
+
+    depends_on('cetmodules', type='build')
+
+    def cmake_args(self):
+        args = ['-Dproduct=trace',
+                '-Dtrace_include_dir={0}'.
+                format(self.spec.prefix.include),
+               ]
+        return args
