44dff89d0a4dfd543a002d04c43be3d5c7aa4d98
Chris Green <artists@fnal.gov>
2019-04-25 16:37:30 -0500

    Add version 1.01.00.

--- packages/cetmodules/package.py
+++ packages/cetmodules/package.py
@@ -15,6 +15,7 @@ class Cetmodules(CMakePackage):
 
     version('develop', branch='develop', git=homepage)
     version('master', branch='master', git=homepage)
+    version('1.01.00', sha256='80bf9a77852751f90b1446e5d5966bea9b73d5b35e024195b35c851dc93a5443', extension='tbz2')
     version('1.00.00', sha256='67438bb1ee9acdaadb6e245ff670463c4603c3283c69b3c9d1a04d6ee9a3fd16', extension='tbz2')
     version('0.07.00', '60fb6f9ff26605ea4c0648fa43d0a516', extension='tbz2')
 
