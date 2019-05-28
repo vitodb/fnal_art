3600bbdd48b1500f4eea05535b10d5590fcc87b1
Chris Green <artists@fnal.gov>
2019-05-28 11:40:02 -0500

    log4cpp: correct inheritance.

--- packages/log4cpp/package.py
+++ packages/log4cpp/package.py
@@ -7,7 +7,7 @@
 from spack import *
 import os, glob
 
-class Log4cpp(Package):
+class Log4cpp(AutotoolsPackage):
     """A library of C++ classes for flexible logging to files (rolling),
 syslog, IDSA and other destinations. It is modeled after the Log for Java
  library (http://www.log4j.org), staying as close to their API as is 
@@ -19,12 +19,6 @@ reasonable."""
     version('1.1.3', '74f0fea7931dc1bc4e5cd34a6318cd2a51322041',extension='tar.gz')
 
     def patch(self):
-        if os.path.exists('./config/config.sub'):
-            os.remove('./config/config.sub')
-            install(os.path.join(os.path.dirname(__file__), '../../config/config.sub'), './config/config.sub')
-        if os.path.exists('./config/config.guess'):
-            os.remove('./config/config.guess')
-            install(os.path.join(os.path.dirname(__file__), '../../config/config.guess'), './config/config.guess')
         patch('patch/log4cpp.patch')
 
     variant('cxxstd',
@@ -33,50 +27,17 @@ reasonable."""
             multi=False,
             description='Use the specified C++ standard when building.')
 
-    def set_cxxstdflag(self):
-        cxxstd = self.spec.variants['cxxstd'].value
-        cxxstdflag = ''
-        if cxxstd == '98':
-            cxxstdflag = self.compiler.cxx98_flag
-        elif cxxstd == '11':
-            cxxstdflag = self.compiler.cxx11_flag
-        elif cxxstd == '14':
-            cxxstdflag = self.compiler.cxx14_flag
-        elif cxxstd == '17':
-            cxxstdflag = self.compiler.cxx17_flag
-        elif cxxstd == 'default':
-            pass
-        else:
-            # The user has selected a (new?) legal value that we've
-            # forgotten to deal with here.
-            tty.die(
-                "INTERNAL ERROR: cannot accommodate unexpected variant ",
-                "cxxstd={0}".format(spec.variants['cxxstd'].value))
-        return cxxstdflag
-
-
     def setup_environment(self, spack_env, run_env):
-        spack_env.append_flags('CXXFLAGS', self.set_cxxstdflag())
-
+        cxxstd = self.spec.variants['cxxstd'].value
+        cxxstdflag = '' if cxxstd == 'default' else \
+                     getattr(self.compiler, 'cxx{0}_flag'.format(cxxstd))
+        spack_env.append_flags('CXXFLAGS', cxxstdflag)
 
     def setup_dependent_environment(self, spack_env, run_env, dspec):
         spack_env.set('LOG4CPP_INC', '{0}'.format(self.prefix.include))
         spack_env.set('LOG4CPP_LIB', '{0}'.format(self.prefix.lib))
-        # Ensure we can find plugin libraries.
-        spack_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
-        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
         spack_env.prepend_path('PATH', self.prefix.bin)
         run_env.prepend_path('PATH', self.prefix.bin)
         spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
         run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
-
-
-    def install(self,spec,prefix):
-        with working_dir(join_path(self.stage.path,'spack-build'), create=True):
-           args = ['--prefix={0}'.format(prefix)]
-           configure=which(join_path(self.stage.source_path,'configure'))
-           configure(*args)
-           make()
-           make('check')
-           make('install')
 
