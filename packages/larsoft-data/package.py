f2e87869e6ebdf3fbc361b5d7e0af5e0ac8124d8
Chris Green <artists@fnal.gov>
2019-06-19 14:42:17 -0500

    Set FW_SEARCH_PATH as appropriate; don't use share.

--- packages/larsoft-data/package.py
+++ packages/larsoft-data/package.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-
+import os
 
 class LarsoftData(Package):
     """LarsoftData"""
@@ -18,6 +18,20 @@ class LarsoftData(Package):
 
     def install(self, spec, prefix):
         install_tree('{0}/v{1}'.format(self.stage.source_path,self.version.underscored),
-                     '{0}'.format(prefix.share))
+                     '{0}'.format(prefix))
 
+    def _add_paths_to_environment(self, env):
+        for path_fragment in ('uboone', 'Argoneut', 'Genfit',
+                              'ParticleIdentification',
+                              os.path.join('pdf', 'Gaisser'),
+                              os.path.join('pdf', 'MUSUN'),
+                              'SupernovaNeutrinos', ''):
+            path = os.path.join(self.prefix, path_fragment)
+            env.prepend_path('FW_SEARCH_PATH', path)
 
+    def setup_environment(self, spack_env, run_env):
+        self._add_paths_to_environment(run_env)
+
+    def setup_dependent_environment(self, spack_env, run_env, dep_spec):
+        self._add_paths_to_environment(spack_env)
+        self._add_paths_to_environment(run_env)
