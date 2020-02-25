d4a27678a12db6658086343bd9bf19729357f9f7
mengel <mengel@fermicloudmwm02.fnal.gov>
2020-02-25 14:36:28 -0600

    icarusutil recipe

---
+++ packages/icarusutil/package.py
@@ -0,0 +1,84 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+import os
+
+def sanitize_environments(*args):
+    for env in args:
+        for var in ('PATH', 'CET_PLUGIN_PATH',
+                    'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH', 'LIBRARY_PATH',
+                    'CMAKE_PREFIX_PATH', 'ROOT_INCLUDE_PATH'):
+            env.prune_duplicate_paths(var)
+            env.deprioritize_system_paths(var)
+
+
+class Icarusutil(CMakePackage):
+    """icarusutil """
+
+    homepage = "http://cdcvs.fnal.gov/redmine/projects/icarusutil"
+    url      = "http://cdcvs.fnal.gov/projects/icarusutil"
+
+    version('08.36.00', tag='v08_36_00', git='http://cdcvs.fnal.gov/projects/icarusutil')
+    version('08.39.00', tag='v08_39_00', git='http://cdcvs.fnal.gov/projects/icarusutil')
+    version('08.41.00', tag='v08_41_00', git='http://cdcvs.fnal.gov/projects/icarusutil')
+    version('08.43.00', tag='v08_43_00', git='http://cdcvs.fnal.gov/projects/icarusutil')
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    depends_on('art-root-io')
+    depends_on('larbatch')
+    depends_on('py-pycurl')
+    depends_on('cetmodules', type='build')
+
+    patch("icarusutil.unups.patch")
+
+    def cmake_args(self):
+        args = ['-DCMAKE_CXX_STANDARD={0}'.
+                format(self.spec.variants['cxxstd'].value)
+               ]
+        return args
+
+    def setup_environment(self, spack_env, run_env):
+        # Binaries.
+        spack_env.prepend_path('PATH',
+                               os.path.join(self.build_directory, 'bin'))
+        spack_env.prepend_path('PYTHONPATH',
+                               os.path.join(self.build_directory, 'bin'))
+        spack_env.prepend_path('PYTHONPATH',
+                               os.path.join(self.build_directory, 'python'))
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH',
+                               os.path.join(self.build_directory, 'lib'))
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Ensure Root can find headers for autoparsing.
+        for d in self.spec.traverse(root=False, cover='nodes', order='post',
+                                    deptype=('link'), direction='children'):
+            spack_env.prepend_path('ROOT_INCLUDE_PATH',
+                                   str(self.spec[d.name].prefix.include))
+            run_env.prepend_path('ROOT_INCLUDE_PATH',
+                                 str(self.spec[d.name].prefix.include))
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Perl modules.
+        sanitize_environments(spack_env, run_env)
+
+    def setup_dependent_environment(self, spack_env, run_env, dspec):
+        spack_env.set('ICARUSUTIL_INC',self.prefix.include)
+        spack_env.set('ICARUSUTIL_LIB', self.prefix.lib)
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        spack_env.prepend_path('PATH', self.prefix.bin)
+        run_env.prepend_path('PATH', self.prefix.bin)
+        spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        spack_env.append_path('FHICL_FILE_PATH','{0}/job'.format(self.prefix))
+        run_env.append_path('FHICL_FILE_PATH','{0}/job'.format(self.prefix))
+        spack_env.append_path('FW_SEARCH_PATH','{0}/gdml'.format(self.prefix))
+        run_env.append_path('FW_SEARCH_PATH','{0}/gdml'.format(self.prefix))
