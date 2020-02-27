b74bcd6f3bdbd3213df22d358f799d4f38e1ea1a
Marc Mengel <mengel@fnal.gov>
2020-02-27 16:21:57 -0600

    adding icaruscode bits

---
+++ packages/icaruscode/package.py
@@ -0,0 +1,118 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+import os
+
+
+def sanitize_environments(*args):
+    for env in args:
+        for var in ('PATH', 'CET_PLUGIN_PATH',
+                    'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH', 'LIBRARY_PATH',
+                    'CMAKE_PREFIX_PATH', 'ROOT_INCLUDE_PATH'):
+            env.prune_duplicate_paths(var)
+            env.deprioritize_system_paths(var)
+
+
+class Icaruscode(CMakePackage):
+    """The eponymous package of the Icarus experiment
+    framework for particle physics experiments.
+    """
+
+    homepage = 'https://cdcvs.fnal.gov/redmine/projects/icaruscode"
+    git_base = 'http://cdcvs.fnal.gov/projects/icaruscode'
+
+    version('develop', branch='develop', git=git_base)
+    version('08.43.00', tag='v08_43_00', git=git_base)
+    version('08.41.00', tag='v08_41_00', git=git_base)
+    version('08.40.00', tag='v08_40_00', git=git_base)
+    version('08.39.00', tag='v08_39_00', git=git_base)
+
+    patch('icaruscode.unups.patch')
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    # Build-only dependencies.
+    depends_on('cmake@3.11:')
+    depends_on('cetmodules@1.01.01:', type='build')
+
+    # Build and link dependencies.
+    depends_on('art_root_io', type=('build','run'))
+    depends_on('art', type=('build','run'))
+    depends_on('boost', type=('build','run'))
+    depends_on('eigen', type=('build','run'))
+    depends_on('ifdh_art', type=('build','run'))
+    depends_on('larana', type=('build','run'))
+    depends_on('larcoreobj', type=('build','run'))
+    depends_on('larcore', type=('build','run'))
+    depends_on('lardataobj', type=('build','run'))
+    depends_on('lardata', type=('build','run'))
+    depends_on('larevt', type=('build','run'))
+    depends_on('larpandora', type=('build','run'))
+    depends_on('larreco', type=('build','run'))
+    depends_on('larsim', type=('build','run'))
+    depends_on('nug4', type=('build','run'))
+    depends_on('nurandom', type=('build','run'))
+    depends_on('nutools', type=('build','run'))
+    depends_on('postgresql', type=('build','run'))
+    depends_on('cppsgl', type=('build','run'))
+
+    if 'SPACKDEV_GENERATOR' in os.environ:
+        generator = os.environ['SPACKDEV_GENERATOR']
+        if generator.endswith('Ninja'):
+            depends_on('ninja', type='build')
+
+    def url_for_version(self, version):
+        url = 'http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/{0}.v{1}.tbz2'
+        return url.format(self.name, version.underscored)
+
+    def cmake_args(self):
+        # Set CMake args.
+        args = ['-DCMAKE_CXX_STANDARD={0}'.
+                format(self.spec.variants['cxxstd'].value)]
+        return args
+
+    def setup_environment(self, spack_env, run_env):
+        # Binaries.
+        spack_env.prepend_path('PATH',
+                               os.path.join(self.build_directory, 'bin'))
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH',
+                               os.path.join(self.build_directory, 'lib'))
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Ensure Root can find headers for autoparsing.
+        for d in self.spec.traverse(root=False, cover='nodes', order='post',
+                                    deptype=('link'), direction='children'):
+            spack_env.prepend_path('ROOT_INCLUDE_PATH',
+                                   str(self.spec[d.name].prefix.include))
+            run_env.prepend_path('ROOT_INCLUDE_PATH',
+                                 str(self.spec[d.name].prefix.include))
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB',
+                               os.path.join(self.build_directory, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleaup.
+        sanitize_environments(spack_env, run_env)
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        # Binaries.
+        spack_env.prepend_path('PATH', self.prefix.bin)
+        run_env.prepend_path('PATH', self.prefix.bin)
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Ensure Root can find headers for autoparsing.
+        spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleanup.
+        sanitize_environments(spack_env, run_env)
