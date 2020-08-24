d7d4d6c182fa7d59d3136ce537e3b84adfcdc839
Marc Mengel <mengel@fnal.gov>
2020-08-24 14:00:59 -0500

    first draft of dunetpc recipe

---
+++ packages/dunetpc/package.py
@@ -0,0 +1,110 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+import os
+
+def sanitize_environments(*args):
+    for env in args:
+        for var in ('PATH', 'CET_PLUGIN_PATH',
+                    'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH', 'LIBRARY_PATH',
+                    'CMAKE_PREFIX_PATH', 'ROOT_INCLUDE_PATH'):
+            env.prune_duplicate_paths(var)
+            env.deprioritize_system_paths(var)
+
+class Dunetpc(CMakePackage):
+    """The eponymous package of the Icarus experiment
+    framework for particle physics experiments.
+    """
+
+    homepage = 'https://cdcvs.fnal.gov/redmine/projects/dunetpc'
+    git_base = 'https://cdcvs.fnal.gov/projects/dunetpc'
+
+    version('develop', branch='develop', git=git_base)
+    version('08.57.00', tag='v08_57_00', git=git_base)
+    version('08.59.00', tag='v08_59_00', git=git_base)
+    version('08.62.00', tag='v08_62_00', git=git_base)
+    version('08.62.01', tag='v08_62_01', git=git_base)
+
+    patch('dunetpc.unups.patch', when='@:08.61.00')
+    patch('dunetpc.08.62.00.patch',when='@08.62.00:')
+
+    variant('cxxstd',
+            default='17',
+            values=('14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    # Build-only dependencies.
+    depends_on('cmake@3.11:')
+    depends_on('cetmodules@1.01.01:', type='build')
+
+    # Build and link dependencies.
+    depends_on('artdaq-core', type=('build','run'))
+    depends_on('dune_oslibs', type=('build','run'))
+    depends_on('duneutil', type=('build','run'))
+    depends_on('boost', type=('build','run'))
+    depends_on('dune_pardata', type=('build','run'))
+    depends_on('dune_raw_data', type=('build','run'))
+    depends_on('genie_phyopt', type=('build','run'))
+    depends_on('hep_hpc', type=('build','run'))
+    depends_on('larsoft', type=('build','run'))
+    depends_on('nusystematics', type=('build','run'))
+    depends_on('systematicstools', type=('build','run'))
+
+    if 'SPACKDEV_GENERATOR' in os.environ:
+        generator = os.environ['SPACKDEV_GENERATOR']
+        if generator.endswith('Ninja'):
+            depends_on('ninja', type='build')
+
+    def url_for_version(self, version):
+        url = 'https://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/{0}.v{1}.tbz2'
+        #url = 'https://github.com/Dune/{0}/archive/v{1}.tar.gz'
+        return url.format(self.name, version.underscored)
+
+    def cmake_args(self):
+        # Set CMake args.
+        args = ['-DCMAKE_CXX_STANDARD={0}'.
+                format(self.spec.variants['cxxstd'].value)]
+        return args
+
+    def setup_environment(self, spack_env, run_env):
+        # Binaries.
+        spack_env.prepend_path('PATH',
+                               os.path.join(self.build_directory, 'bin'))
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH',
+                               os.path.join(self.build_directory, 'lib'))
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Ensure Root can find headers for autoparsing.
+        for d in self.spec.traverse(root=False, cover='nodes', order='post',
+                                    deptype=('link'), direction='children'):
+            spack_env.prepend_path('ROOT_INCLUDE_PATH',
+                                   str(self.spec[d.name].prefix.include))
+            run_env.prepend_path('ROOT_INCLUDE_PATH',
+                                 str(self.spec[d.name].prefix.include))
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB',
+                               os.path.join(self.build_directory, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleaup.
+        sanitize_environments(spack_env, run_env)
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        # Binaries.
+        spack_env.prepend_path('PATH', self.prefix.bin)
+        run_env.prepend_path('PATH', self.prefix.bin)
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Ensure Root can find headers for autoparsing.
+        spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleanup.
+        sanitize_environments(spack_env, run_env)
