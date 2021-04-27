bd6e9667fcc733b98f424d11a6b1ee0eb8ce4202
Marc Mengel <mengel@fnal.gov>
2021-04-27 19:13:14 +0000

    getting closer to larsoft

--- packages/h5cpp/package.py
+++ packages/h5cpp/package.py
@@ -6,15 +6,20 @@
 from spack import *
 import os
 import sys
+import re
 
 class H5cpp(Package):
     """Hierarchical Data Format C++ templates for Serial and Paralell HDF5"""
 
     homepage = "https://h5cpp.org/"
-    url      = "https://h5cpp.org/download/h5cpp-full_1.10.4.1.tar.gz"
+    url      = "https://github.com/steven-varga/h5cpp/archive/refs/tags/v1.10.4-6.tar.gz"
 
-    version('1.10.4.1')
+    version('1.10.4.6', sha256='4fbc8e777dc78a37ec2fe8c7b6a47114080ffe587f083e83a2046b5e794aef93')
+    version('1.10.4.5', sha256='661ccc4d76e081afc73df71ef11d027837d92dd1089185f3650afcaec9d418ec')
+
+    def url_for_version(self, version):
+        return "https://github.com/steven-varga/h5cpp/archive/refs/tags/v{0}.tar.gz".format(re.sub("\.([0-9])$", "-\\1", str(version)))
 
     def install(self, spec, prefix):
-        install_tree(self.stage.source_path + "/usr/include", prefix+"/include")
+        install_tree(self.stage.source_path + "/h5cpp", prefix+"/include/h5cpp")
 
