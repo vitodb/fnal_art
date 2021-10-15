f8de8f542f99032e6d6705d15c60d39dc97ac521
Patrick Gartung <gartung@fnal.gov>
2021-10-15 09:15:20 -0500

    update checksums for cetmodules and cetbuildtools

--- packages/cetbuildtools/package.py
+++ packages/cetbuildtools/package.py
@@ -21,21 +21,46 @@
 # ----------------------------------------------------------------------------
 
 from spack import *
+from llnl.util import tty
+import sys
+import os
+import spack.util.spack_json as sjson
 
 
 class Cetbuildtools(CMakePackage):
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://cdcvs.fnal.gov/redmine/projects/cetbuildtools"
-    url = 'https://github.com/art-framework-suite/{0}/archive/refs/tags/v{0}.tar.gz'
+    git_base = 'https://github.com/art-framework-suite/cetbuildtools'
+    url = 'https://github.com/art-framework-suite/cetbuildtools/archive/refs/tags/v8_15_00.tar.gz'
+    list_url = 'https://api.github.com/repos/art-framework-suite/cetbuildtools/tags'
 
+
+    version('8.18.05', sha256='009a82d56f2149399de45eb81d3ed021b83b3e8c8c3fcc4f3db13b7d49d51759')
+    version('8.18.04', sha256='6ef9838ceeb361d41a36edc9c83b6c7ce13899ea543022cb94cac4df00386aad')
+    version('8.18.03', sha256='de4db6e4066668f24620d5ca1a8f00036c5afdb65f8a649855ee75e9fe7232a4')
+    version('8.18.02', sha256='9e6de0eeadb1def81903cbb026b8141482cbc828f6e29bcb69344f6540c144b2')
+    version('8.18.01', sha256='335f31eea442a1e22d17e3ac37a112bf8524f8ce161d1ebe04fff22fa21a4327')
+    version('8.18.00', sha256='d31652b2002ce2b198cf274ba76448783ee1f6c4d3b076086bb07edb0c28db7f')
+    version('8.17.03', sha256='c2e15d06d80a81f1763318c612580d8b855847ebd5683056edb9d66185b98ea0')
+    version('8.17.02', sha256='78722424bccafa6de2d651395daf3b737112c7f83a9ab22ac087cccd36e90982')
+    version('8.17.01', sha256='b9e04af24359716c13fbe4d2459134fa78297dbd14bb0b2134499c0755b534b9')
+    version('8.17.00', sha256='fe27ba75e08f225fafa22f852ec4c4587a214e4114e1fc8779d6827137c6a5a0')
+    version('8.15.00', sha256='e1560da61d83e190c6d7a25068696479d99d067a53700caa6c072f5a986caa83')
     version('8.06.00', sha256='eeceb410c6ec710c384ea4b3bca4d02adc8b6d8c84886d9d3647204c32d3d8ef')
-    version('8.15.00', sha256='b9cd2ee60f282716b9cd42a3a569c25e549a6a3fae3d1fc95b845187a05ce13f')
 
     def url_for_version(self, version):
-        #url = 'https://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/{0}.v{1}.tbz2'
-        url = 'https://github.com/art-framework-suite/{0}/archive/refs/tags/v{1}.tar.gz'
+        url = 'https://github.com/art-framework-suite/{0}/archive/v{1}.tar.gz'
         return url.format(self.name, version.underscored)
+
+    def fetch_remote_versions(self, concurrency=None):
+        return dict(map(lambda v: (v.dotted, self.url_for_version(v)),
+                        [ Version(d['name'][1:]) for d in
+                          sjson.load(
+                              spack.util.web.read_from_url(
+                                  self.list_url,
+                                  accept_content_type='application/json')[2])
+                          if d['name'].startswith('v') ]))
 
     depends_on('cetmodules@2:')
 
--- packages/cetmodules/package.py
+++ packages/cetmodules/package.py
@@ -15,19 +15,23 @@ class Cetmodules(CMakePackage):
 
     homepage = 'https://cdcvs.fnal.gov/redmine/projects/cetmodules'
     git_base = 'https://github.com/FNALssi/cetmodules.git'
-    #url = 'https://github.com/FNALssi/cetmodules/archive/refs/tags/2.14.00.tar.gz'
     url = 'https://github.com/FNALssi/cetmodules/archive/refs/tags/2.29.04.tar.gz'
 
     version('develop', branch='develop', git=git_base, get_full_repo=True)
 
-    #version('2.25.01', sha256='cb58c8a759589e08dcd11e6a9bf6341a130d18e2cbe53abc359a7514aacba5b2')
-
+    version('2.29.07', sha256='372a0f2c46df49c9d86633dbc11bf69213d9749945f160a1f8b50a7badf81e99')
+    version('2.29.06', sha256='8eefc5f18b2c094c4d784c0c025cde88c09a9b3bf2cca5e28b256a079ad20fa3')
+    version('2.29.05', sha256='35e70edd7126b66c22982fd442df61d0d7439c72760642ac920c6e76a87c8767')
+    version('2.29.04', sha256='3042ae45814f9c196d085abed8e6f0dbc1d2c0e8c61c1e457de1b9711b32b715')
+    version('2.29.03', sha256='e40485a6cc7170a51669656901a956896d237f778f26f7f3e62671d092c7bfd5')
+    version('2.29.02', sha256='519a7c685122c0cab8b699773785f15b3e05292fb566db950478b6506bfbb065')
+    version('2.29.00', sha256='74af89f2540b1e116207dc278ab77df19cfac959dfbf64055ba975563979d1c1')
+    version('2.28.03', sha256='ae1a451862eab9fdc992e5c918b626c26a431af90f25b66cbc535492c96529a0')
+    version('2.28.02', sha256='8abcdd4ea6b4c3a3325cd2c080e00adccb402531ed336351db2b592ef830b442')
+    version('2.28.01', sha256='8afafa7d995c29d694775afeecb5c2ffb513d3e266cba8ca01ec0eae82173de2')
     version('2.16.02', sha256='43082380b23b3367303368b6ec698d7a3624b19a8a99842752bd13bcb474625e')
-
     version('2.14.00', sha256='360b719133d644d47f092f42895f3037891cfb30adf6897472f62f037a3129f1')
-
     version('2.09.00', sha256='8c4d9a5f3d39a7dff5e9136576dab1bcf50a410c6cc028e5b47a2546c57e3860')
-
     version('1.04.00', sha256='4ba077e7146e19a805476f2fcf2a537a2cd24dbd602f5f5c348431b5018379da')
     version('1.03.00', sha256='40b0f8fa88f9522d14cae87a4242a494aa724b13505b6b740a0007153cc85e18')
     version('1.02.04', sha256='4e5063748aa8821b34a2a3ae150927afcd890beade83ad98e03f93e5d93d8ee7')
@@ -40,7 +44,6 @@ class Cetmodules(CMakePackage):
     version('1.01.01', sha256='0b7e28d4c29c9941a6960609e8ab4aa33f90736ef1dc81fc7b1b520e99ee0ae7')
     version('1.01.00', sha256='80bf9a77852751f90b1446e5d5966bea9b73d5b35e024195b35c851dc93a5443')
     version('1.00.00', sha256='67438bb1ee9acdaadb6e245ff670463c4603c3283c69b3c9d1a04d6ee9a3fd16')
-    version('0.07.00', md5='60fb6f9ff26605ea4c0648fa43d0a516')
 
     depends_on('cmake@3.11:', type='build', when='@:1.01.99')
     depends_on('cmake@3.12:', type='build', when='@1.02.00:')
