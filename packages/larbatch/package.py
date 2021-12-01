89427f24aff60cb4f9ccde18c9acbb33d4cee9a9
Giuseppe Cerati <cerati@fnal.gov>
2021-12-01 08:35:14 -0800

    fixes for v09_35_00

--- packages/larbatch/package.py
+++ packages/larbatch/package.py
@@ -13,7 +13,7 @@ class Larbatch(Package):
     homepage = "https://cdcvs.fnal.gov/redmine/projects/larbatch-web-client/wiki"
     url      = "https://github.com/LArSoft/larbatch/archive/refs/tags/v01_51_15.tar.gz"
 
-    version('01_51_15', sha256='485bf5f4d7ae12eec396e7d44418a92b21a9f65cebcc6d1868ba9c3b4e8a074c')
+    version('01_51_15', sha256='609c85155d4368fd82f21cbb1b68b4f56556c4580e9ab6f36efa3f29ece02d0a')
     version('01.51.12', sha256='5ab54efd1897eeab66663a7029a9b0781b691051d0a47998b5989acbeb5117c3')
     version('01.51.11', sha256='e2336eb76b63e6aa95dc24d86ec6b777b31d7ed08cd45e331bcbee050e9e1018')
     version('01.51.10', sha256='da3c92246716484ba563147deefc4da878824d8109a8dd22d8877ed7926733ef')
