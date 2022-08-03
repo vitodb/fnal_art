5765c14bd2a48e5a50a9cfa86719f14d4524d4a7
Chris Green <greenc@fnal.gov>
2022-08-03 15:20:29 -0500

    py-jltheme: remove redundant/faulty hook ensure_readme_rst()
    * Numbered versions of py-jltheme have `README.rst`.
    
    * Class `PythonPackage` has no `build` phase: should have been
      `install`.

--- packages/py-jltheme/package.py
+++ packages/py-jltheme/package.py
@@ -19,11 +19,3 @@ Adjusts Matplotlib axis labels, edge, face, and tick colors based upon which Jup
     version('0.1.2', sha256='75361cbd59c835d7d71b8c9c23dd9ddf4644bf67d7e2a42afa260de7c39aa028')
 
     depends_on('py-jupyter', type=('run'))
-
-    @run_before('build')
-    def ensure_readme_rst(self):
-        with working_dir(self.stage.source_path):
-            if not os.path.exists("README.rst"):
-                f = open("README.rst","w")
-                f.write("description")
-                f.close()
